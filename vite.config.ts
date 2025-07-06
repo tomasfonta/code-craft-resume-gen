c821ec1460eae289b02ebcffcd76f3aa9a2683da
tomas fontanarrosa <tomasfontanarrosa@tomass-MacBook-Air.local>
2025-07-06 10:28:42 +0200

    Changed Base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/code-craft-resume-gen/",
+  base: "/tf/",
   server: {
     host: "::",
     port: 8080,
