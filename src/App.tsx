f9069168cb70b18c3530bba51c24a25af3d3ee37
tomas fontanarrosa <tomasfontanarrosa@tomass-MacBook-Air.local>
2025-07-06 09:50:08 +0200

    add base url

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+const { BASE_URL } = import.meta.env;
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+
+
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path={BASE_URL} element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path={`${BASE_URL}*`} element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/code-craft-resume-gen/",
   server: {
     host: "::",
     port: 8080,
